6d45007cbe579e587dd0a428dbda72f958e8efa3
thomascvieira <81846236+thomascvieira@users.noreply.github.com>
2021-10-04 18:18:22 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <div className="background">
+      <div className="container">
+        <Header />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+const Header = () => {
+    return (
+        <header>
+          <h1 className="title">Heavy Weather</h1>
+          <input placeholder="Enter ZIP code (ex 90210)"></input>
+          
+        </header>
+    )
+}
+
+export default Header
