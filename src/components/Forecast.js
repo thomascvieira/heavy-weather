caa40fa80da266abbfae1d4e50afdec7786381ef
thomascvieira <81846236+thomascvieira@users.noreply.github.com>
2021-10-13 13:43:38 -0400

    mapping forecast data
    Mapping forecast data hourly/daily works. Renamed components to make more sense.

--- src/components/Forecast.js
+++ src/components/Forecast.js
@@ -1,10 +1,44 @@
-import ForecastSection from "./ForecastSection"
-const Forecast = ({weatherData, forecastType, forecast}) => {
-    return (
-        <>
-        {forecast.map((forecastSection) => (<ForecastSection key={forecastSection.dt} forecastSection={forecastSection}/>))}
-        </>
-    )
+import Projection from "./Projection"
+const Forecast = ({ weatherData, forecastType }) => {
+    // checks for API response
+    if (weatherData[0] !== 0) {
+
+        if (forecastType === 'hourly') {
+            let data = weatherData.hourly
+            return (
+                <>
+                    {data.slice(0,9).map((projection) =>
+                    (<Projection
+                        key={projection.dt}
+                        time={Intl.DateTimeFormat('en-US', { hour: 'numeric', hour12: 'true' }).format(projection.dt * 1000)}
+                        temp={projection.temp}
+                    />)
+                    )}
+                </>
+            )
+        }
+        if (forecastType === 'daily') {
+            let data = weatherData.daily
+            return (
+                <>
+                    {data.map((projection) => 
+                    (<Projection 
+                        key={projection.dt} 
+                        time={Intl.DateTimeFormat('en-US', { weekday: 'short', day: 'numeric'}).format(projection.dt * 1000)} 
+                        temp={projection.temp.day} />)
+                    )}
+                </>
+            )
+        }
+    }
+    // while waiting for API response 
+    else {
+        return (
+            <>
+            </>
+        )
+    }
+
 }
 
 export default Forecast
---
+++ src/components/Projection.js
@@ -0,0 +1,9 @@
+const Projection = ({time, temp}) => {
+    return (
+        <div>
+           <p>{time}&#9;{temp}</p> 
+        </div>
+    )
+}
+
+export default Projection
